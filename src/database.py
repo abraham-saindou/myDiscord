c3bff933fc13ff71791cb455812c65843c219da7
Abraham-Saindou <abraham.saindou@laplateforme.io>
2023-03-20 13:42:33 +0100

    first commit on my branch.

--- src/database.py
+++ src/database.py
@@ -2,5 +2,14 @@ import mysql.connector
 
 
 class SQL:
-    def __init__(self, nom, prenom, mail, mdp):
-        pass
+    def __init__(self, hote, utilisateur, mdp, database):
+        self.connector = mysql.connector.connect()
+        self.cursor = self.connector.cursor()
+
+    def new_user(self, nom, prenom, mail, mdp):
+        insert = "INSERT INTO utilsateurs(nom, prenom, email, mdp)"
+        self.cursor.execute(insert)
+
+    def add_message(self):
+        message = "INSERT INTO messages(id_auteur, date_pub, texte)"
+        self.cursor.execute(message)
