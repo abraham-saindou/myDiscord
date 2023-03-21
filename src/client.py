6fbd3f357a00ca71236736821a2e119f971edfbe
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2023-03-21 13:12:52 +0100

    client

--- src/client.py
+++ src/client.py
@@ -12,7 +12,7 @@ PORT = 33000
 db = mysql.connector.connect(
         host = "localhost",
         user = "root",
-        password = "SuperP3scado",
+        password = "",
         database = "Boutique"
 )
 
